781287e47e0c8256d0732470af9f35e24adf6ddc
Viktor N <152975192+pads-oculars@users.noreply.github.com>
2024-01-11 13:17:23 +0700

    Create Order.vb

---
+++ Order.vb
@@ -0,0 +1,23 @@
+' Order.vb
+
+Public Class Order
+    Private _orderId As Integer
+    Private _products As List(Of Product)
+
+    Public Sub New(orderId As Integer)
+        _orderId = orderId
+        _products = New List(Of Product)()
+    End Sub
+
+    Public Sub AddProduct(product As Product)
+        _products.Add(product)
+    End Sub
+
+    Public Sub Display()
+        Console.WriteLine($"Order ID: {_orderId}")
+        Console.WriteLine("Products in order:")
+        For Each product In _products
+            product.Display()
+        Next
+    End Sub
+End Class
